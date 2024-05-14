9eba626b7ebc6e4369c74f00942ed3eb7006c4de
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2024-05-14 12:24:39 +0200

    updated test for update_graph

--- server/app/tests/test_routers.py
+++ server/app/tests/test_routers.py
@@ -18,6 +18,6 @@ def test__read_root__redirected() -> None:
 
 
 def test__update_graph__redirected() -> None:
-    response = client.patch("/api/v0/graph", json={})
+    response = client.patch("/api/v0/graph", json={"sparql": "PREFIX ns1: <http://dellemc.com:8080/icv/> PREFIX schema: <https://schema.org/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> DELETE DATA {  <urn:ngsi-ld:Vehicle:5FSQC8LARN> ns1:driverSeatLocation 'Right'^^schema:steeringPosition .}"})
     assert response.status_code == HTTP_200_OK
     assert response.json() == "Success"
