f915c7b264fa810eaee593c36157bdbba2524b79
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2024-08-06 12:46:19 +0200

    Adding data to 'slice' dataset instead of 'cheese'

--- server/app/routers.py
+++ server/app/routers.py
@@ -23,7 +23,7 @@ router = APIRouter(tags=[TagEnum.GRAPH])
 
 fuseki_jena_url = "jena-fuseki.integration"
 fuseki_jena_port = None
-fuseki_jena_dataset_name = "cheese"
+fuseki_jena_dataset_name = "slice"
 fuseki = FusekiCommunicatior(
     fuseki_jena_url, fuseki_jena_port, fuseki_jena_dataset_name
 )
