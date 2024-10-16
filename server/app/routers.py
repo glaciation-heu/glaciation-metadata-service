7691acb68c3d8f2ab22e641563d7fc8f8b1ebf93
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2024-10-16 15:15:28 +0200

    returning to single update query

--- server/app/routers.py
+++ server/app/routers.py
@@ -47,7 +47,6 @@ async def update_graph(
     body: UpdateRequestBody,
 ) -> str:
     """Update Distributed Knowledge Graph"""
-    TRIPLES_LIMIT = 100
     g = Graph()
     g.parse(StringIO(dumps(body)), format="json-ld")
 
@@ -55,30 +54,22 @@ async def update_graph(
 
     query = "INSERT DATA {\n"
     query += "\tGRAPH <timestamp:%d> {\n" % ts
-    i = 0
     for s, p, o in g.triples((None, None, None)):
         if hasattr(s, "n3") and hasattr(p, "n3") and hasattr(o, "n3"):
             query += f"\t\t{s.n3()} {p.n3()} {o.n3()} .\n"
-            i += 1
         else:
             return "Failure"
+    query += "\t}\n"
+    query += "}"
 
-        if i == TRIPLES_LIMIT:
-            query += "\t}\n"
-            query += "}"
+    valid, msg = fuseki.validate_sparql(query, "update")
 
-            valid, msg = fuseki.validate_sparql(query, "update")
-
-            if valid:
-                fuseki.update_query(query)
-            else:
-                print(msg)
-                print(f"The query:\n{query}")
-                return "Failure"
-
-            query = "INSERT DATA {\n"
-            query += "\tGRAPH <timestamp:%d> {\n" % ts
-            i = 0
+    if valid:
+        fuseki.update_query(query)
+    else:
+        print(msg)
+        print(f"The query:\n{query}")
+        return "Failure"
 
     return "Success"
 
