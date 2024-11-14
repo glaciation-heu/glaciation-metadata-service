3d80511a87a21a2ecaea6e006e06e1324265a589
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2024-11-14 16:55:19 +0100

    sending drop queries one by one

--- server/app/drop_graphs.py
+++ server/app/drop_graphs.py
@@ -3,9 +3,9 @@
 
 from typing import Any, Dict
 
-import time
 from datetime import datetime, timedelta
 from re import search
+from time import sleep
 from urllib.parse import urlencode
 
 import requests
@@ -62,15 +62,13 @@ def job():
     cutoff_time = datetime.now() - timedelta(days=1)
     cutoff_timestamp = int(cutoff_time.timestamp()) * 1000
 
-    query = ""
     for graphURI in timestamps:
         if timestamps[graphURI] < cutoff_timestamp:
-            if len(query) > 0:
-                query += "\n"
-            query += f"DROP GRAPH <{graphURI}>;"
+            query = f"DROP GRAPH <{graphURI}>;"
 
-    local_query(query, True)
-    logger.info(f"Performed:\n{query}")
+            local_query(query, True)
+            logger.info(f"Dropped graph <{query}>.")
+            sleep(2)
 
 
 # schedule.every().day.at("23:59").do(job)
@@ -79,4 +77,4 @@ schedule.every().minute.at(":30").do(job)
 if __name__ == "__main__":
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        sleep(1)
